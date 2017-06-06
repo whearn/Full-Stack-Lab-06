a7b888f74193c7ad00cdb4489532440b8557d8eb
whearn <wchearn00@gmail.com>
2017-06-06 13:30:02 -0400

    Added Documentation for styels.js, and Added Styling for index.html

--- script.js
+++ script.js
@@ -7,19 +7,29 @@ var items = ['frisbee', 'paper clip', 'hair brush', 'vaccuum cleaner', 'pillow',
             'CD', 'record', 'book', 'stuffed animal', 'action figure'];
 
 function createAccusation(num) {
+    //returns a function that creates each accusation
     return function() {
+        //creates an index for each array that will not exceed that array's length
         var friendIndex = num % 5;
         var locationsIndex = num % 10;
         var itemsIndex = num % 20;
 
+        //pieces each accusation alert together using different names, locations, and items
         alert('Accusation ' + num + ': I accuse ' + friends[friendIndex] + ', with the ' + items[itemsIndex] + ' in the ' + locations[locationsIndex] + '!');
     }
 }
 
 for (var i = 1; i <= 100; i++) {
+    //creates each accusation's h3 element
     var accusationList = document.createElement('h3');
     accusationList.innerHTML = 'Accusation #' + i;
+
+    //assign the createAccusation function to a variable
     var accusation = createAccusation(i);
+
+    //pass the createAccusation function into the h3 click event listener
     accusationList.addEventListener('click', accusation);
+
+    //adds an h3 to the body with each loop
     document.body.appendChild(accusationList);
 }
